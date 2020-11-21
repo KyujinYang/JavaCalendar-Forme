fbd54811299295f4f6d6b1e701b05c3549ab5828
USER <USER@DESKTOP-1F88JRD>
2020-11-21 15:41:42 +0900

    tip2. scanner 자동입력 명령어 ctrl+shitf+n (import java.util.Scanner;)

--- src/calendar_20201121_Twosome/Calendar.java
+++ src/calendar_20201121_Twosome/Calendar.java
@@ -1,9 +1,15 @@
 package calendar_20201121_Twosome;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,Calendar");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		//scanner 자동으로 갖고오는 명령어 Ctrl+shift+n => improt java.util.scanner
+		Scanner scannecr = new Scanner();
 		
 
 	}
