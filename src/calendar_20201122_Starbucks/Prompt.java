acd792a115ddea2742ba9602ff679ab0c255ca6e
USER <USER@DESKTOP-1F88JRD>
2020-11-22 15:55:54 +0900

    step6. Line break, output of leap calendar

--- src/calendar_20201122_Starbucks/Prompt.java
+++ src/calendar_20201122_Starbucks/Prompt.java
@@ -1,4 +1,4 @@
-//년도입력되게 추가해줬어요~ step6. 하드코딩(PROMPT 이용 달력 출력)
+//step6. PROMPT 이용 예뿌게 변경~ 행바꿈\n, 윤달
 
 package calendar_20201122_Starbucks;
 
@@ -11,24 +11,27 @@ public class Prompt {
 
 	public void runPrompt() {
 		Scanner scan = new Scanner(System.in);
-		//Repeatitive_while_if cal = new Repeatitive_while_if();
+		// Repeatitive_while_if cal = new Repeatitive_while_if();
 		Virtual_calendar_output cal = new Virtual_calendar_output();
 
 		int year = 5;
 		int month = 3;
 
-		//step6. 년도도 입력되게 출력!
+		// step6. 년도도 입력되게 출력!
 		while (true) {
-			System.out.println("년도을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.println("\n년도을 입력하세요");
+			System.out.print("YEAR>  ");
+			
 			year = scan.nextInt();
 			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scan.nextInt();
+			System.out.print("MONTH>  ");
+			month = scan.nextInt() ;
 
+			
 			// -1 이면 루프를 중지시키고 메시지를 출력한다.
 			if (month == -1) {
 				System.out.printf("이만 가보겠습니다~총총총");
+				
 				break;
 			}
 			// step4. -1이면 이 무한루프를 빠져나와라
@@ -36,22 +39,18 @@ public class Prompt {
 				continue;
 			}
 
-			cal.printCalendar(year, month); 
+			cal.printCalendar(year, month);
 
-			
-	
 		}
-		
+
 		scan.close();
 	}
-	
 
 	public static void main(String[] args) {
 
 		// 셀 실행
 		Prompt P = new Prompt();
 		P.runPrompt();
-		
-		
+
 	}
 }
