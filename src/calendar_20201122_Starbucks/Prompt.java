e860545ae4ef06c828fcfc69bf8f0c5b96b7b02b
USER <USER@DESKTOP-1F88JRD>
2020-11-22 15:23:40 +0900

    step6.Add Year Input(PROMPT hard coding)

--- src/calendar_20201122_Starbucks/Prompt.java
+++ src/calendar_20201122_Starbucks/Prompt.java
@@ -1,3 +1,5 @@
+//년도입력되게 추가해줬어요~ step6. 하드코딩(PROMPT 이용 달력 출력)
+
 package calendar_20201122_Starbucks;
 
 import java.util.Scanner;
@@ -12,10 +14,14 @@ public class Prompt {
 		//Repeatitive_while_if cal = new Repeatitive_while_if();
 		Virtual_calendar_output cal = new Virtual_calendar_output();
 
+		int year = 5;
 		int month = 3;
 
+		//step6. 년도도 입력되게 출력!
 		while (true) {
-
+			System.out.println("년도을 입력하세요");
+			System.out.print(PROMPT);
+			year = scan.nextInt();
 			System.out.println("달을 입력하세요");
 			System.out.print(PROMPT);
 			month = scan.nextInt();
@@ -30,7 +36,7 @@ public class Prompt {
 				continue;
 			}
 
-			cal.printCalendar(2020, month); 
+			cal.printCalendar(year, month); 
 
 			
 	
--- src/calendar_20201122_Starbucks/Virtual_calendar_output.java
+++ src/calendar_20201122_Starbucks/Virtual_calendar_output.java
@@ -1,17 +1,32 @@
 package calendar_20201122_Starbucks;
 
-
 public class Virtual_calendar_output {
 
 	// 함수를 만들어준다! public int maxDaysOfMonth(int month) {
-	private static final int[] MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getmaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+			return true;
+		else
+			return false;
 	}
+
+	public int getmaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
 	/*
-	 * 월 입력을 하면 해당 월의 달력을 출력한다. 달력 모양은 1단계에서 작성한 모양으로 만든다. 1일은 일요일로 정해도 무방하다. -1을
-	 * 입력받기 전까지 반복 입력받는다. 프롬프트를 출력한다.
+	 * - 월 입력을 하면 해당 월의 달력을 출력한다. 
+	 * - 달력 모양은 1단계에서 작성한 모양으로 만든다. 
+	 * - 1일은 일요일로 정해도 무방하다. 
+	 * - -1을 입력받기 전까지 반복 입력받는다. 
+	 * - 프롬프트를 출력한다.
 	 */
 
 	public void printCalendar(int year, int month) {
@@ -19,14 +34,12 @@ public class Virtual_calendar_output {
 		System.out.println("SU  MO  TU  WE  TH  FR  SA");
 		System.out.println("--------------------------");
 
-		int maxDay = getmaxDaysOfMonth(month);
+		int maxDay = getmaxDaysOfMonth(year, month);
 
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if (i % 7 == 0)
 				System.out.println();
-
-			
 
 		}
 
